047f0352e928ab695049c24b66f34397de988f7f
Ashur Cabrera <hello@ashur.cab>
2022-03-19 14:56:57 -0600

    Update debug logging

--- actions/debug/index.cjs
+++ actions/debug/index.cjs
@@ -1,2 +1,25 @@
-console.log("process.env:", process.env);
-console.log("__dirname", __dirname);
+const fs = require("fs");
+const path = require("path");
+
+let directoryPaths = [
+	path.join(__dirname, '../../'),
+	path.join(__dirname, '../'),
+	path.join(__dirname, './'),
+];
+
+directoryPaths.forEach( directoryPath => lsDir(directoryPath) );
+
+function lsDir(directoryPath) {
+	fs.readdir(directoryPath, function (err, files) {
+		console.log(directoryPath);
+
+		if (err) {
+			return console.log('Unable to scan directory: ' + err);
+		}
+
+		files.forEach(function (file) {
+			console.log(` - ${file}`);
+		});
+	});
+}
+
