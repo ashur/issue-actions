3bc5baead4e34eade30b274c24e6fdafcbda9c08
Ashur Cabrera <hello@ashur.cab>
2022-03-21 21:39:27 -0600

    Wrap with async IIFE

--- actions/issue-link/index.js
+++ actions/issue-link/index.js
@@ -6,34 +6,34 @@ import * as github from "@actions/github";
 import {parseIssueBody} from "../../src/parse-issue-body.js";
 import {writeData} from "../../src/write-data.js";
 
-try {
-	/* Parse issue */
-	const issue = github.context.payload.issue;
-	const issueData = parseIssueBody(getInput("issue-template"), issue.body);
-	console.log({issueData});
+(async() => {
+	try {
+		/* Parse issue */
+		const issue = github.context.payload.issue;
+		const issueData = await parseIssueBody(getInput("issue-template"), issue.body);
+		const {hostname, pathname} = new URL(issueData.url);
 
-	const {hostname, pathname} = new URL(issueData.url);
+		/* Write to disk*/
+		const date = new Date(issue['created_date']);
+		const outputDir = path.join(
+			getInput("folder"),
+			date.getFullYear().toString(),
+			(date.getMonth() + 1).toString().padStart(2, "0"),
+		);
 
-	/* Write to disk*/
-	const date = new Date(issue['created_date']);
-	const outputDir = path.join(
-		getInput("folder"),
-		date.getFullYear().toString(),
-		(date.getMonth() + 1).toString().padStart(2, "0"),
-	);
+		let hash = createHash("sha256");
+		hash.update(pathname);
 
-	let hash = createHash("sha256");
-	hash.update(pathname);
+		let outputFilename = hostname;
+		outputFilename += pathname.length > 1 ? `-${hash.digest("base64url").substr(0, 10)}` : '';
+		outputFilename += ".json";
 
-	let outputFilename = hostname;
-	outputFilename += pathname.length > 1 ? `-${hash.digest("base64url").substr(0, 10)}` : '';
-	outputFilename += ".json";
+		writeData(outputDir, outputFilename);
 
-	writeData(outputDir, outputFilename);
-
-	exportVariable("IssueNumber", issue.number);
-}
-catch (error) {
-	console.log(error);
-	setFailed(error.message);
-}
+		exportVariable("IssueNumber", issue.number);
+	}
+	catch (error) {
+		console.log(error);
+		setFailed(error.message);
+	}
+})();
