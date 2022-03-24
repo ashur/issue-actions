937098bf0e774bcfb1aca4bb1a08324e7509dc42
Ashur Cabrera <hello@ashur.cab>
2022-03-23 22:08:17 -0600

    Expand IssueParser tests

--- test/issue-parser.js
+++ test/issue-parser.js
@@ -17,5 +17,25 @@ describe('IssueParser', () =>
 			assert.equal(bodyData.url, 'https://example.com', 'url');
 			assert.equal(bodyData.description, '> This domain is for use in illustrative examples in documents.', 'description');
 		});
+
+		it('should support multiple paragraphs in a section', async () => {
+			const issueParser = new IssueParser(issueTemplatesDir);
+			const issueBody = '### Description\n\nI have eaten\nthe plums\nthat were in\nthe icebox\n\nand which\nyou were probably\nsaving\nfor breakfast';
+
+			const bodyData = await issueParser.parseBody(issueBody, 'template.yml');
+
+			assert.isObject(bodyData);
+			assert.equal(bodyData.description, 'I have eaten\nthe plums\nthat were in\nthe icebox\n\nand which\nyou were probably\nsaving\nfor breakfast', 'description');
+		});
+
+		it('should support formatted text', async () => {
+			const issueParser = new IssueParser(issueTemplatesDir);
+			const issueBody = '### Description\n\nLorem ipsum _dolor_ **sit amet**.';
+
+			const bodyData = await issueParser.parseBody(issueBody, 'template.yml');
+
+			assert.isObject(bodyData);
+			assert.equal(bodyData.description, 'Lorem ipsum _dolor_ **sit amet**.', 'description');
+		});
 	});
 });
